7c10747e35ff14760bd5a856d7f90ef2a1baab42
Inokcode <regilearn@gmail.com>
2022-02-22 18:24:14 +0530

    logout btn added

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ yarn dev
 
 yarn add tailwindcss postcss autoprefixer
 
-yarn add ethers
+yarn add ethers(interact with block chain and smart contracts)
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,24 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 const Navbar = () => {
+
+
+  const NavbarItem = ({title, classProps}) => {
+    return (
+      <li className={`mx-4 cursor-pointer ${classProps}`}>
+          {title}
+      </li>
+    )
+  }
+
   return (<nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-start">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market","Exchange","Tutorials","Wallets"].map((item)=>(   <NavbarItem key={item} title={item}/>))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">Login</li>
+      </ul>
   </nav>);
 };
 
